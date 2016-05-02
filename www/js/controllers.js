6ba8a909e278d5ee70d20bf0f59bd9ecbee55dff
Sharon Desmond <mathemagiks@gmail.com>
2016-05-02 21:17:16 +0100

    Create skeleton alternative camera implementation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,26 +16,64 @@ angular.module('starter.controllers', [])
     Chats.remove(chat);
   };
 })*/
-.controller('cameraCtrl', function($scope, Camera) {
- $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
-      console.log(imageURI);
-      $scope.lastPhoto = imageURI;
-    }, function(err) {
-      console.err(err);
-    }, {
-      quality: 75,
-      targetWidth: 320,
-      targetHeight: 320,
-      saveToPhotoAlbum: true
-    });
-  };
+.controller('cameraCtrl', function($scope, $cordovaCamera) {
 
+ $scope.takePhoto = function () {
+      var options = {
+        quality: 75,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        //popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: true
+    };
 
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+            // An error occured. Show a message to the user
+        });
+    }
+    
+    $scope.choosePhoto = function () {
+      var options = {
+        quality: 75,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        //popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: true
+    };
+
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+            // An error occured. Show a message to the user
+        });
+    }
 
 
 })
-   
+  
+ // $scope.getPhoto = function() {
+ //    Camera.getPicture().then(function(imageURI) {
+ //      console.log(imageURI);
+ //      $scope.lastPhoto = imageURI;
+ //    }, function(err) {
+ //      console.err(err);
+ //    }, {
+ //      quality: 75,
+ //      targetWidth: 320,
+ //      targetHeight: 320,
+ //      saveToPhotoAlbum: true
+ //    });
+ //  };
 .controller('galleryCtrl', function($scope) {
 
 })
